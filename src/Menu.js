f1f883cb2f2a5968367f23f6cdc167245d0813d3
Matdrox <Matdrox@users.noreply.github.com>
2022-12-20 14:05:10 +0100

    [Updated] Menu Buttons

--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 
 const Menu = () => {
+  const [selectedButton, setSelectedButton] = useState(null);
+
+  const handleButtonClick = (num) => {
+    setSelectedButton(num);
+  };
+
   return (
     // <div>
     //   <div className='flex-row'>
@@ -15,18 +21,25 @@ const Menu = () => {
     //   </div>
     // </div>
     <div>
-      <p className='text-center text-4xl p-5 text-zinc-400'>SELECT NUMBER OF PLAYERS</p>
-      <div className='grid grid-cols-6 gap-4'>
-        {[6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17].map((number) => (
+      <p className='text-center text-4xl p-5 text-zinc-600'>
+        SELECT NUMBER OF PLAYERS
+      </p>
+      <div className='grid grid-cols-4 gap-4 md:grid-cols-6'>
+        {[6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17].map((num) => (
           <motion.button
-            key={number}
+            key={num}
             type='button'
-            className='col-span-1 w-24 h-24 text-4xl rounded-md bg-purple-600 shadow-lg shadow-purple-400 text-white font-bold'
+            className={`col-span-1 w-24 h-24 text-4xl rounded-md shadow-lg text-white font-bold ${
+              num === selectedButton
+							? 'bg-orange-500 shadow-orange-300'
+							: 'bg-purple-600 shadow-purple-400'
+            }`}
             whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 1, transition: {duration: 0.03} }}
-			transition={{duration: 0.1}}
+            whileTap={{ scale: 1, transition: { duration: 0.03 } }}
+            transition={{ duration: 0.1 }}
+            onClick={() => handleButtonClick(num)}
           >
-            {number}
+            {num}
           </motion.button>
         ))}
       </div>
